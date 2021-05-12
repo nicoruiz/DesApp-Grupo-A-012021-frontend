d5ad651e28473d7e8ce150b259510a6ad8c3fe50
Nicolás Ruiz <nruiz@hexacta.com>
2021-05-11 21:21:33 -0300

    Undo test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders re-seña title', () => {
+  render(<App />);
+  const titleElement = screen.getByText(/Re-seña!/i);
+  expect(titleElement).toBeInTheDocument();
+});
